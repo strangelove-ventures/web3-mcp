5cca76eb6ab2ef6850a5180c7ae4f8a125f55800
Alex Cheng <alexander.cheng@utexas.edu>
2025-02-24 14:03:50 -0600

    crosschain bridges and swaps aggregation through Rubic

--- src/general/index.ts
+++ src/general/index.ts
@@ -1,6 +1,8 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { registerCoinGeckoTools } from "./coingecko.js";
+import { registerRubicTools } from "./rubic.js";
 
 export function registerGeneralTools(server: McpServer) {
   registerCoinGeckoTools(server);
+  registerRubicTools(server);
 }
